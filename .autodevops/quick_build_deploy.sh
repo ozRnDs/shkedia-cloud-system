1782d2e10c09d95b191e312751231ad1e3add12e
oz <oz.rnds@gmail.com>
2023-12-04 22:42:51 +0200

    ci(.autodevops,-docker-compose.yaml): Create deploy script

---
+++ .autodevops/quick_build_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#USAGE Example bash .autodevops/quick_build_deploy.sh dev auth
+
+export ENVIRONMENT=$1
+
+echo Start CD process to $ENVIRONMENT environment
+source .autodevops/SERVICE_VERSIONS.properties
+
+docker compose up -d
