7801d49878405cee9163d9a22aa36fcf810af2c6
oz <oz.rnds@gmail.com>
2024-01-08 00:32:22 +0200

    feat(MonthWorker): Add months-worker and upload options to the project

--- .autodevops/quick_build_deploy.sh
+++ .autodevops/quick_build_deploy.sh
@@ -3,9 +3,16 @@
 #USAGE Example bash .autodevops/quick_build_deploy.sh dev auth
 
 export ENVIRONMENT=$1
+export STATIC=$2
 
 echo Start CD process to $ENVIRONMENT environment
 source .autodevops/SERVICE_VERSIONS.properties
 source .local/important_variables.properties
 
+if [ "$STATIC" == "MOVE" ]; then
+    echo Copy static
+    sudo cp -r $SOURCE_LOCATION $TARGET_LOCATION
+fi
+
+echo Start Containers
 docker compose up -d
